6ba4beb71f3c0115d3beb7feb647a7d8c692cce5
David Seidenberg <david.seidenberg@pubnub.com>
2020-08-19 09:54:34 -0700

    Refactor to move parser to separate file using module

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
-use std::io::{BufReader, BufRead};
-use std::fs::File;
+
+mod parser;
 use std::env;
 
 fn main() {
@@ -8,7 +8,7 @@ fn main() {
     let args: Vec<String> = env::args().collect();
 
     println!("Parsing assembly file: {}", &args[1]);
-    let mut file_parser = Parser::new(&args[1]);
+    let mut file_parser = parser::new(&args[1]);
 
     while file_parser.has_more_commands() {
         file_parser.advance();
@@ -20,8 +20,8 @@ fn main() {
         println!("{:?}", command_type);
 
         match command_type {
-            Command::ACommand | Command::LCommand => println!("Symbol {}", file_parser.symbol()),
-            Command::CCommand => {
+            parser::Command::ACommand | parser::Command::LCommand => println!("Symbol {}", file_parser.symbol()),
+            parser::Command::CCommand => {
                 println!("Dest {:?}", file_parser.dest());
                 println!("Comp {:?}", file_parser.comp());
                 println!("Jump {:?}", file_parser.jump());
@@ -31,125 +31,3 @@ fn main() {
 
 }
 
-#[derive(Debug)]
-enum Command {
-    ACommand,
-    CCommand,
-    LCommand,
-}
-
-struct Parser {
-    lines: Vec<String>,
-    current_command_index: Option<usize>,
-    current_command: Option<String>,
-}
-
-impl Parser {
-    fn new(filename: &str) -> Parser {
-        let file = File::open(filename).unwrap();
-        let reader = BufReader::new(file);
-
-        let mut lines = Vec::new();
-
-        for line in reader.lines() {
-            if let Ok(line) = line {
-
-                if line.starts_with("//") {
-                    continue;
-                }
-
-                if line.is_empty() {
-                    continue;
-                }
-
-                lines.push(line);
-            }
-        }
-
-        Parser{
-            lines,
-            current_command_index: None,
-            current_command: None,
-        }
-    }
-
-    fn has_more_commands(&self) -> bool {
-        match self.current_command_index {
-            Some(index) => index < self.lines.len(),
-            None => true,
-        }
-    }
-
-    fn advance(&mut self) {
-        match self.current_command_index {
-            Some(index) => {
-                self.current_command_index = Some(index+1);
-            },
-            None => {
-                self.current_command_index = Some(0);
-            }
-        }
-
-        if self.has_more_commands() {
-            let command_ref = &self.lines[self.current_command_index.unwrap()];
-            self.current_command = Some(String::from(command_ref));
-        }
-    }
-
-    fn command_type(&self) -> Command {
-
-        let command = self.current_command.as_ref().unwrap();
-        if command.starts_with("@") {
-            return Command::ACommand;
-        }
-
-        if command.contains("=") || command.contains(";") {
-            return Command::CCommand;
-        }
-
-        Command::LCommand
-    }
-
-    fn symbol(&self) -> String {
-        let command = self.current_command.as_ref().unwrap();
-        match self.command_type() {
-            Command::ACommand => command.trim_start_matches("@").to_string(),
-            Command::LCommand => command.to_string(),
-            _ => command.to_string(),
-        }
-    }
-
-    fn dest(&self) -> Option<String> {
-        let command = self.current_command.as_ref().unwrap();
-
-        if command.contains("=") {
-            return Some(command.split("=").collect::<Vec<&str>>().first().unwrap().to_string());
-        }
-
-        None
-    }
-
-    fn comp(&self) -> Option<String> {
-        let mut command = self.current_command.as_ref().unwrap().clone();
-
-        if command.contains("=") {
-            command = command.split("=").collect::<Vec<&str>>().last().unwrap().to_string();
-        }
-
-        if command.contains(";") {
-            command = command.split(";").collect::<Vec<&str>>().first().unwrap().to_string();
-        }
-
-        Some(command.to_string())
-    }
-
-    fn jump(&self) -> Option<String> {
-        let command = self.current_command.as_ref().unwrap();
-
-        if command.contains(";") {
-            return Some(command.split(";").collect::<Vec<&str>>().last().unwrap().to_string());
-        }
-
-        None
-    }
-}
---
+++ src/parser.rs
@@ -0,0 +1,128 @@
+use std::fs::File;
+use std::io::{BufReader, BufRead};
+
+
+#[derive(Debug)]
+pub enum Command {
+    ACommand,
+    CCommand,
+    LCommand,
+}
+
+pub struct Parser {
+    lines: Vec<String>,
+    current_command_index: Option<usize>,
+    pub current_command: Option<String>,
+}
+
+pub fn new(filename: &str) -> Parser {
+    let file = File::open(filename).unwrap();
+    let reader = BufReader::new(file);
+
+    let mut lines = Vec::new();
+
+    for line in reader.lines() {
+        if let Ok(line) = line {
+
+            if line.starts_with("//") {
+                continue;
+            }
+
+            if line.is_empty() {
+                continue;
+            }
+
+            lines.push(line);
+        }
+    }
+
+    Parser{
+        lines,
+        current_command_index: None,
+        current_command: None,
+    }
+}
+
+impl Parser {
+    pub fn has_more_commands(&self) -> bool {
+        match self.current_command_index {
+            Some(index) => index < self.lines.len(),
+            None => true,
+        }
+    }
+
+    pub fn advance(&mut self) {
+        match self.current_command_index {
+            Some(index) => {
+                self.current_command_index = Some(index+1);
+            },
+            None => {
+                self.current_command_index = Some(0);
+            }
+        }
+
+        if self.has_more_commands() {
+            let command_ref = &self.lines[self.current_command_index.unwrap()];
+            self.current_command = Some(String::from(command_ref));
+        }
+    }
+
+    pub fn command_type(&self) -> Command {
+
+        let command = self.current_command.as_ref().unwrap();
+        if command.starts_with("@") {
+            return Command::ACommand;
+        }
+
+        if command.contains("=") || command.contains(";") {
+            return Command::CCommand;
+        }
+
+        Command::LCommand
+    }
+
+    pub fn symbol(&self) -> String {
+        let command = self.current_command.as_ref().unwrap();
+        match self.command_type() {
+            Command::ACommand => command.trim_start_matches("@").to_string(),
+            Command::LCommand => command.to_string(),
+            _ => command.to_string(),
+        }
+    }
+
+    pub fn dest(&self) -> Option<String> {
+        let command = self.current_command.as_ref().unwrap();
+
+        if command.contains("=") {
+            return Some(command.split("=").collect::<Vec<&str>>().first().unwrap().to_string());
+        }
+
+        None
+    }
+
+    pub fn comp(&self) -> Option<String> {
+        let mut command = self.current_command.as_ref().unwrap().clone();
+
+        if command.contains("=") {
+            command = command.split("=").collect::<Vec<&str>>().last().unwrap().to_string();
+        }
+
+        if command.contains(";") {
+            command = command.split(";").collect::<Vec<&str>>().first().unwrap().to_string();
+        }
+
+        Some(command.to_string())
+    }
+
+    pub fn jump(&self) -> Option<String> {
+        let command = self.current_command.as_ref().unwrap();
+
+        if command.contains(";") {
+            return Some(command.split(";").collect::<Vec<&str>>().last().unwrap().to_string());
+        }
+
+        None
+    }
+}    
+
+
